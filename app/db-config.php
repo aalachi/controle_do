06fa3263acda09fc19bb676928243e03fa1e259d
a3lachi <mad.aalachi@gmail.com>
2026-01-02 18:29:36 +0100

    init: first commit

---
+++ app/db-config.php
@@ -0,0 +1,15 @@
+<?php
+const DB_DSN = 'mysql:host=db;dbname=test';
+const DB_USER = "test";
+const DB_PASS = "test";
+
+
+
+$options = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", 
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
+    PDO::ATTR_EMULATE_PREPARES => false 
+);
+
+
+docker update --restart=no
